d04f8561a9c15e738e4db6f26a3c07d8425231bc
osam98714-boop <osam98714@gmail.com>
2026-07-17 13:43:14 +0300

    Add files via upload

---
+++ plugins/auto/noBots.js
@@ -0,0 +1,11 @@
+export default async function before(m, { conn, bot }) {
+    const groups = [
+        "@g.us"
+    ]; /* حط الجروبات الي عايز البوتات الفرعي متشتغلش فيها */
+
+    if (bot.isSubBot && groups.includes(m.chat)) {
+        return true;
+    }
+
+    return false;
+}
