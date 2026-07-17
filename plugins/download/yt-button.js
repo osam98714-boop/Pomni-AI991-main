6ac27fe7cc17570de0e68db9224c253efddff79b
osam98714-boop <osam98714@gmail.com>
2026-07-17 13:45:33 +0300

    Add files via upload

---
+++ plugins/download/yt-button.js
@@ -0,0 +1,26 @@
+const handler = async (m, { conn, text }) => {
+    if (!text) return m.reply("💙 ~ اكتب نص الفيديو او الاغنيه ~ ❤️");
+    
+    const res = await fetch(`https://emam-api.web.id/home/sections/Search/api/YouTube/search?q=${text}`);
+    const { data } = await res.json();
+    const { title, image, timestamp: time, url } = data[0];
+
+    await conn.sendButton(m.chat, {
+        imageUrl: image,
+        bodyText: `${title} ╎ ${time}`,
+        footerText: "🕸️ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ~ 𝐘𝐨𝐮𝐓𝐮𝐛𝐞 🕸️",
+        buttons: [
+            { name: "quick_reply", params: { display_text: "🎼 ╎ تـحـمـيـل صـوت", id: `.يوت_اغنيه ${url}` } },
+            { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
+        ],
+        mentions: [m.sender],
+        newsletter: { name: "𝑳𝑭990 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🇸🇦", jid: "" },
+        interactiveConfig: { buttons_limits: 10, list_title: "𝑳𝑭 🎀", button_title: "𝑳𝑭 🎀", canonical_url: url }
+    }, m);
+};
+
+handler.usage = ["فيديو", "اغنيه", "شغل"];
+handler.category = "downloads";
+handler.command = ["اغنيه", "فيديو", "اغنية", "play", "video"];
+
+export default handler;
